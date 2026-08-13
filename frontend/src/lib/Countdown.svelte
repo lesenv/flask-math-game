0c35a26af2e81ba0066b55ff59a1299c67b88488
origin-unknown <ich.hier.wer.dort@gmx.de>
2026-08-13 23:45:11 +0200

    Game end after 10 rounds added. Users are shown the final game score and given the option to replay.

--- frontend/src/lib/Countdown.svelte
+++ frontend/src/lib/Countdown.svelte
@@ -2,21 +2,21 @@
 
 	let { display } = $props();
 
-    let count = $state(10);
+	let count = $state(10);
 
-    $effect(() => {
-        if (count <= 0) return;
+	$effect(() => {
+		if (count <= 0) return;
 
-        const timeout = setTimeout(() => {
-            count--;
-        }, 1000);
+		const timeout = setTimeout(() => {
+			count--;
+		}, 1000);
 
-        return () => clearTimeout(timeout);
-    });
+		return () => clearTimeout(timeout);
+	});
 </script>
 
 {#if count > 0}
-    <h1>{count}</h1>
+	<h1>{count}</h1>
 {:else}
-    {@render display()}
+	{@render display()}
 {/if}
