fb47351138fb7047c520bd2e6682c0c61bc8ad65
lesenv <johndoe@example.com>
2026-08-16 16:06:44 +0200

    'tasks' division is now inheriting kleines_1x1, no `MAX_TASK_ATTEMPTS = 3` needed

--- backend/src/tasks.py
+++ backend/src/tasks.py
@@ -1,7 +1,5 @@
 import random
 
-MAX_TASK_ATTEMPTS = 3
-
 class Task():
     ''' parent class for all the different Tasks available
     '''
@@ -35,35 +33,21 @@ class kleines_1x1(Task):
             'str': self.string()
         }
 
-class Division(Task):
+class Division(kleines_1x1, Task):
     def __init__(self, a = None, b = None):
-        if not a:
-            a = random.randint(1,10)
-        if not b:
-            b = random.randint(1,10)
-        self._assign_self_variables(a, b)
-
-    def _assign_self_variables(self, a, b):
-        self.x = a * b
-        self.y = b
-        self.z = a
-
-    def new_task(self):
-        a, b = random.randint(1, 10), random.randint(1, 10)
-        self._assign_self_variables(a, b)
-        return a, b
+        super().__init__(a=a, b=b)
 
     def string(self):
-        return f"{self.x} / {self.y}"
+        return f"{self.c} / {self.b}"
 
     def check(self, num):
-        return num == self.z
+        return num == self.a
 
     def _asdict(self):
         return {
-            'a': self.x,
-            'b': self.y,
-            'c': self.z,
+            'a': self.c,
+            'b': self.b,
+            'c': self.a,
             'str': self.string()
         }
 
