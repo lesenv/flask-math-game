377fd50e14af48eeec50d2933a50bf823cd7eec1
lesenv <johndoe@example.com>
2026-08-16 12:49:58 +0200

    get_2_ints removed

--- backend/src/tasks.py
+++ backend/src/tasks.py
@@ -2,10 +2,6 @@ import random
 
 MAX_TASK_ATTEMPTS = 3
 
-def get_2_ints(_from: int = 1, _to: int = 10) -> list[int]:
-    if _from > _to: _from, _to = _to, _from
-    return [random.randint(_from, _to), random.randint(_from, _to)]
-
 class Task():
     ''' parent class for all the different Tasks available
     '''
