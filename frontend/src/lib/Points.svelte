0f44d30a7732d06fc6a846b1f8eb23211691ab82
lesenv <johndoe@example.com>
2026-08-19 22:54:06 +0200

    showing hearts instead of the numbers

--- frontend/src/lib/Points.svelte
+++ frontend/src/lib/Points.svelte
@@ -2,4 +2,5 @@
 	let { points } = $props();
 </script>
 
-<div>Points: <span>{points}</span></div>
+<div>Leben: <span>
+	{#each { length: points/10 }}<img src="../../public/heart 2.jpg" width="40px" alt="X" \>{/each}</span></div>
